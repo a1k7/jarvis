22349d3121a63b6958a505534571f870f2eca4b4
akhil <warik.akhilesh@gmail.com>
2024-11-23 19:24:05 +0530

    day2 complete

--- www/main.js
+++ www/main.js
@@ -12,4 +12,29 @@ $(document).ready(function () {
     
     
     });
+    // siri configuration//
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 840,
+        height: 600,
+        style:"ios9",
+        amplitude:"1",
+        speed:"0.30",
+        autostart:true
+
+      });
+      // siri-message animation//
+      $('.siri-message').textillate({
+        loop:true,
+        sync:true,
+        in:{
+            effect:"fadeInUp",
+            sync:true,
+
+        },
+        out:{
+            effect:"fadeOutUp",
+            sync:true,
+        },
+      });
 });
